e8a58adc537d6805972804b8661e7bc77061a234
dev1980 <brham1980@gmail.com>
2020-09-24 17:04:40 +0545

    add style to the page

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, {useEffect, useState} from 'react';
 import Movie from './components/Movie'
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5fe0af2a35351d345ad67d45a80457e9&page=1";
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie/343611?api_key= 5fe0af2a35351d345ad67d45a80457e9&query="
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key= 04c35731a5ee918f014970082a0088b1&query=";
 
 function App() {
 const [movies, setMovies ] = useState([]);
+const [searchTerm, setSearchTerm] = useState('');
 
 useEffect(() => {
  fetch(FEATURED_API)
@@ -17,13 +18,46 @@ useEffect(() => {
    console.log(data)
    setMovies(data.results)
  });
-}, [])
+}, []);
+
+const handelOnSubmit = (e) => {
+  e.preventDefault();
+
+  if(searchTerm){
+    fetch(SEARCH_API + searchTerm)
+    .then(res => res.json())
+    .then((data) => {
+      console.log(data)
+      setMovies(data.results)
+    });
+
+    setSearchTerm('');
+  }
+};
+
+const handleOnChange = (e) => {
+  setSearchTerm(e.trarget.value);
+};
   return (
+    <>
+    <header>
+    <form onSubmit={handelOnSubmit}>
+    <input 
+    className="search" 
+    type="search" 
+    placeholder="Search...." 
+    value={searchTerm}
+    onChange={handleOnChange}
+    />
+    </form>
+    </header>
+
     <div className="movie-container">
 
       {movies.length > 0 && movies.map((movie) => 
       <Movie key={movie.id} {...movie} />)}
     </div>
+    </>
   );
 }
 
