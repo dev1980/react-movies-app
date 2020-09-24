ca1adf7ae5381454d4b0b16b7f1ac2bddefadce8
dev1980 <brham1980@gmail.com>
2020-09-24 17:48:03 +0545

    refactor the code and style

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 import React, {useEffect, useState} from 'react';
 import Movie from './components/Movie'
 
-const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=04c35731a5ee918f014970082a0088b1&page=1";
+const FEATURED_API = "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=5fe0af2a35351d345ad67d45a80457e9&page=1";
 
-const IMG_API = "https://image.tmdb.org/t/p/w1280";
-
-const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key= 04c35731a5ee918f014970082a0088b1&query=";
+const SEARCH_API = "https://api.themoviedb.org/3/search/movie?&api_key=5fe0af2a35351d345ad67d45a80457e9&query=";
 
 function App() {
 const [movies, setMovies ] = useState([]);
 const [searchTerm, setSearchTerm] = useState('');
 
 useEffect(() => {
- fetch(FEATURED_API)
- .then(res => res.json())
- .then((data) => {
-   console.log(data)
-   setMovies(data.results)
- });
+getMovies(FEATURED_API)
 }, []);
 
+const getMovies = function(API){
+  fetch(API)
+  .then(res => res.json())
+  .then((data) => {
+    console.log(data)
+    setMovies(data.results)
+  });
+}
 const handelOnSubmit = (e) => {
   e.preventDefault();
 
   if(searchTerm){
-    fetch(SEARCH_API + searchTerm)
-    .then(res => res.json())
-    .then((data) => {
-      console.log(data)
-      setMovies(data.results)
-    });
-
+    getMovies(SEARCH_API + searchTerm)
+   
     setSearchTerm('');
   }
 };
 
 const handleOnChange = (e) => {
-  setSearchTerm(e.trarget.value);
+  setSearchTerm(e.target.value);
 };
   return (
     <>
